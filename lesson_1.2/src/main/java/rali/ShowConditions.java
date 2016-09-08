4c668d8b283edcf7ec15ff35308ed11255803169
antonbb <tohabalbookh@mail.ru>
2016-09-08 20:47:36 +0300

    delete .idea

---
+++ lesson_1.2/src/main/java/rali/ShowConditions.java
@@ -0,0 +1,11 @@
+package rali;
+public class ShowConditions {
+public static void main (String[] args){
+boolean condit=false;
+System.out.println(condit?"this is true":"this is false");
+
+
+
+}
+
+}
